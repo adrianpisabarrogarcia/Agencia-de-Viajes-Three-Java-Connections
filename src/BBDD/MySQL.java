3aef7c7cc7ee056372a65abe6f81d2b66bee271b
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-11-06 13:24:17 +0100

    MySQL conexion ✅

--- src/BBDD/MySQL.java
+++ src/BBDD/MySQL.java
@@ -1,13 +1,95 @@
 package BBDD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQL {
 
-    public MySQL() {
+    public static Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al cargar el driver MySQL");
+            throw new RuntimeException(e);
+        }
 
+        try {
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenciadeviajes", "root", "");
+            //System.out.println("Conectado a la base de datos");
+            return conexion;
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con la base de datos MySQL");
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void conectar() {
+    //Desconectar
+    public static void desconectar(Connection conexion) {
+        try {
+            conexion.close();
+            //System.out.println("Desconectado de la base de datos");
+        } catch (SQLException e) {
+            System.out.println("Error al desconectar de la base de datos MySQL");
+            throw new RuntimeException(e);
+        }
+    }
 
+    //Insertar
+    public static void insertar(String SQL) {
+        try {
+            Connection conexion = conectar();
+            conexion.createStatement().executeUpdate(SQL);
+            System.out.println("Insertado correctamente");
+            desconectar(conexion);
+        } catch (SQLException e) {
+            System.out.println("Error al insertar en MySQL");
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Modificar
+    public static void modificar(String SQL) {
+        try {
+            Connection conexion = conectar();
+            conexion.createStatement().executeUpdate(SQL);
+            System.out.println("Modificado correctamente");
+            desconectar(conexion);
+        } catch (SQLException e) {
+            System.out.println("Error al modificar en MySQL");
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Eliminar
+    public static void eliminar(String SQL) {
+        try {
+            Connection conexion = conectar();
+            conexion.createStatement().executeUpdate(SQL);
+            System.out.println("Eliminado correctamente");
+            desconectar(conexion);
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar en MySQL");
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Consultar
+    public static ResultSet consultar(String SQL) {
+        try {
+            Connection conexion = conectar();
+            ResultSet rs = conexion.createStatement().executeQuery(SQL);
+            System.out.println("Consultado correctamente");
+            desconectar(conexion);
+
+            //Obtener los datos de la consulta
+            return rs;
+
+        } catch (SQLException e) {
+            System.out.println("Error al consultar en MySQL");
+            throw new RuntimeException(e);
+        }
     }
 
 
